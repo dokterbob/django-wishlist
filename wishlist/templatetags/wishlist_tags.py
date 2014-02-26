01c92ddbf8d3cfa9817248763629480afef43a7b
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-02-26 19:18:37 +0100

    Template tags for adding/removing items.

---
+++ wishlist/templatetags/wishlist_tags.py
@@ -0,0 +1,36 @@
+from django import template
+
+register = template.Library()
+
+# from ..settings import wishlist_settings
+
+
+def wishlist_form(context, item):
+    """
+    Render wishlist form for adding or removing a particular item.
+
+    Assumes the user is available in the context and requires an instance of
+    the wishlist model *not* WishlistItem.
+    """
+
+    # TODO: Replace by proper, meaningful, exceptions.
+    assert 'user' in context
+    # assert isinstance(item, wishlist_settings.ITEM_MODEL)
+    assert hasattr(item, 'pk')
+
+    return {
+        'user': context['user'],
+        'item': item
+    }
+
+# Register for adding
+register.inclusion_tag(
+    'wishlist/wishlistitem_add_form.html',
+    takes_context=True, name='wishlist_add_form'
+)(wishlist_form)
+
+# Register for removing
+register.inclusion_tag(
+    'wishlist/wishlistitem_remove_form.html',
+    takes_context=True, name='wishlist_remove_form'
+)(wishlist_form)
