3fa487c75767a85cfeec09bd21dd4d9d4030a047
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-02-27 15:24:23 +0100

    PEP8 of URL's.

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -6,8 +6,12 @@ from .views import (
 
 
 urlpatterns = patterns('',
-    url(r'^$', WishlistView.as_view(), name='wishlist'),
-    url(r'^add/$', WishlistAddView.as_view(), name='wishlist_add'),
-    url(r'^clear/$', WishlistClearView.as_view(), name='wishlist_clear'),
-    url(r'^(?P<pk>\d+)/$', WishlistRemoveView.as_view(), name='wishlist_remove'),
+    url(r'^$',
+        WishlistView.as_view(), name='wishlist'),
+    url(r'^add/$',
+        WishlistAddView.as_view(), name='wishlist_add'),
+    url(r'^clear/$',
+        WishlistClearView.as_view(), name='wishlist_clear'),
+    url(r'^(?P<pk>\d+)/remove/$',
+        WishlistRemoveView.as_view(), name='wishlist_remove'),
 )
