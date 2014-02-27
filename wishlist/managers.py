bdfbd65b50031c9938068b406d423d6ec8eab8b6
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-02-27 23:02:47 +0100

    Compatibility with Django <1.6

--- wishlist/managers.py
+++ wishlist/managers.py
@@ -14,7 +14,13 @@ class UserManager(Manager):
     """ Custom manager adding for_user(user) method. """
 
     def get_queryset(self):
+        """ Return a UserQuerySet instead of QuerySet. """
         return UserQuerySet(self.model, using=self._db)
 
     def for_user(self, user):
+        """ Wrapper around for_user() QS method. """
         return self.get_queryset().for_user(user=user)
+
+    def get_query_set(self):
+        """ Django <1.6 compatibility wrapper. """
+        return self.get_queryset()
